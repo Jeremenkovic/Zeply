be299cd75bc6820fec71e713d5711c6abf25d8ad
Jeremenkovic <jeremenkovic23@gmail.com>
2023-04-14 22:59:07 +0300

    added encription Fernet

--- address_api/models.py
+++ address_api/models.py
@@ -12,6 +12,7 @@ class CryptoAddress(models.Model):
     id = models.AutoField(primary_key=True)
     cryptocurrency = models.CharField(max_length=10, choices=CRYPTO_CHOICES)
     address = models.CharField(max_length=255)
+    encrypted_private_key = models.CharField(max_length=255, default='')
 
     def __str__(self):
         return f"{self.cryptocurrency}: {self.address}"
--- address_api/views.py
+++ address_api/views.py
@@ -9,7 +9,19 @@ from bit import PrivateKeyTestnet
 from bitcash import Key as BCHKey
 from xrpl.wallet import Wallet as XRPWallet
 from stellar_sdk import Keypair as XLMKeypair
+from cryptography.fernet import Fernet
 from django.shortcuts import get_object_or_404
+
+def encrypt_key(key: str) -> str:
+    cipher_suite = Fernet(Fernet.generate_key())
+    cipher_text = cipher_suite.encrypt(key.encode())
+    return cipher_text.decode()
+
+def decrypt_key(encrypted_key: str) -> str:
+    cipher_suite = Fernet(Fernet.generate_key())
+    plain_text = cipher_suite.decrypt(encrypted_key.encode())
+    return plain_text.decode()
+
 
 class GenerateAddressView(generics.CreateAPIView):
     queryset = CryptoAddress.objects.all()
@@ -17,33 +29,39 @@ class GenerateAddressView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         crypto = self.request.data['cryptocurrency']
+        address, encrypted_key = None, None
+
         if crypto == 'BTC':
             private_key = PrivateKeyTestnet()
             address = private_key.address
-            serializer.save(cryptocurrency=crypto, address=address)
-        
+            encrypted_key = encrypt_key(private_key.to_hex())
         elif crypto == 'ETH':
             try:
                 acct = Account.create()
+                private_key = acct.key
                 address = acct.address
-                serializer.save(cryptocurrency=crypto, address=address)
+                encrypted_key = encrypt_key(private_key.hex())
             except Exception as e:
                 return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         elif crypto == 'BCH':
             private_key = BCHKey()
             address = private_key.address
-            serializer.save(cryptocurrency=crypto, address=address)
+            encrypted_key = encrypt_key(private_key.to_hex())
         elif crypto == 'XRP':
             wallet = XRPWallet.create()
+            private_key = wallet.private_key # Set private_key for XRP
             address = wallet.classic_address
-            serializer.save(cryptocurrency=crypto, address=address)
+            encrypted_key = encrypt_key(private_key)
         elif crypto == 'XLM':
-            keypair = XLMKeypair.random()
-            address = keypair.public_key
-            serializer.save(cryptocurrency=crypto, address=address)
+           keypair = XLMKeypair.random()
+           private_key = keypair.secret
+           address = keypair.public_key
+           encrypted_key = encrypt_key(private_key)
         else:
-            return Response({"error": "Invalid cryptocurrency"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Invalid cryptocurrency"}, status_code=status.HTTP_400_BAD_REQUEST)
 
+        if address and encrypted_key:
+            serializer.save(cryptocurrency=crypto, address=address, encrypted_private_key=encrypted_key)
 
 
 
